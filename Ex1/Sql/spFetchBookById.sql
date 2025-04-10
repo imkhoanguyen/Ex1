76d38c6d68b0623091e044c83fa7f6d3a7cb0a6c
imkhoanguyen <khoanguyencoder@gmail.com>
2025-04-10 21:46:32 +0700

    change flie sql

--- Ex1/Sql/spFetchBookById.sql
+++ Ex1/Sql/spFetchBookById.sql
@@ -1,22 +1,5 @@
-CREATE PROCEDURE sp_FilterBooks
-    @SearchKey NVARCHAR(200),
-    @AuthorId INT = NULL,
-    @FromDate DATETIME = NULL,
-    @ToDate DATETIME = NULL,
-    @PageSize INT,  
-    @PageIndex INT 
+CREATE PROCEDURE sp_GetBookById @id INT
 AS
 BEGIN
-    SET NOCOUNT ON;
-   
-    SELECT * 
-    FROM Books
-    WHERE
-        (@SearchKey IS NULL OR Title LIKE '%' + @SearchKey + '%') 
-        AND (@AuthorId IS NULL OR AuthorId = @AuthorId)           
-        AND (@FromDate IS NULL OR PublishedDate >= @FromDate)     
-        AND (@ToDate IS NULL OR PublishedDate <= @ToDate)          
-    ORDER BY BookId DESC 
-    OFFSET (@PageIndex - 1) * @PageSize ROWS  
-    FETCH NEXT @PageSize ROWS ONLY;
+    SELECT * FROM Books WHERE BookId = @id;
 END;
